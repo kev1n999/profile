abdfbe77f74da948f42307c61b6ddb1180575866
kev1n999 <sopratesta999@gmail.com>
2026-02-03 02:21:57 -0300

    fix layout shift

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,11 +5,19 @@ export default function NavBar() {
   return (
     <header>
       <nav className="flex justify-center pt-20">
-        <ul className="flex justify-center gap-3.5">
+        <ul className="flex justify-between gap-3.5">
           <NavItem source="/" content="home" />
           <NavItem source="/skills" content="skills" />
           <NavItem source="/contacts" content="contacts" />
         </ul>
+        <div className="ml-56">
+            <a href="https://github.com/kev1n999"
+              className="
+                text-white relative transition-all before:inline-block before:w-4 before:content-['#']
+                hover:before:content-['~'] text-2xl underline
+              "
+            >github</a>
+        </div>
       </nav>
 
       <div className="bg-white text-white h-0.5 relative top-6 max-w-2xl mx-auto"></div>
@@ -30,8 +38,8 @@ function NavItem({ source, content }: LinkProps) {
       <Link
         to={source}
         className="
-          text-white transition-all before:content-['#']
-          hover:before:content-['~'] text-2xl
+          text-white relative transition-all before:inline-block before:w-4 before:content-['#']
+          hover:before:content-['>'] text-2xl underline
         "
       >
         { content }
